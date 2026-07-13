22177bf4d7480bf69995404d4fe54687716f8444
Pizduk-Lenia <danil.tikhomirov.06@inbox.ru>
2026-07-13 11:20:20 +1000

    новый тест на doker, будет выполняться в ci

---
+++ tests/test_docker_api.py
@@ -0,0 +1,26 @@
+import pytest
+pytestmark = pytest.mark.docker
+
+import pandas as pd
+import requests
+from sklearn.metrics import recall_score, precision_score, accuracy_score
+
+BASE_URL = "http://localhost:8000"
+
+def test_calculate_model_via_docker():
+    tests = pd.read_csv("required_data/tests.csv")
+    X = tests.drop(["Churn"], axis=1)
+    y = tests["Churn"]
+
+    data = X.to_dict(orient="records")
+    response = requests.post(f"{BASE_URL}/predict", json=data)
+    assert response.status_code == 200
+
+    predictions = pd.Series(response.json()["prediction"])
+    RECALL = recall_score(y, predictions, pos_label="Yes")
+    PRECISION = precision_score(y, predictions, pos_label="Yes")
+    ACCURACY = accuracy_score(y, predictions)
+
+    assert RECALL >= 0.6
+    assert PRECISION >= 0.55
+    assert ACCURACY >= 0.75
